f573fc5ec381551859ddc7dcaa21ec5a34cb5ca8
bpricemg <43038187+bpricemg@users.noreply.github.com>
2026-09-22 11:00:23 -0400

    Updated 9/22

---
+++ chapter1/types/types0.c
@@ -0,0 +1,16 @@
+/**
+ * This program will create a variable, assign a value,
+ * and print.
+ */
+
+ int main(void)
+ {
+    // Declare an integer variable called x
+    int x;
+
+    // Assign a value to x
+    x = 13;
+
+    // Print out the current value of x
+    printf("The value of x is %i\n", x);
+ }
